1c77e45dc319848a7b3cc5b33a82699ae2401b05
Benjamin Renoust <renoust@gmail.com>
2012-05-28 23:25:06 -0400

    proof of concept of the interaction between d3 and tulip regarding the selection of a subgraph (new test-file bostockex.htm, derived from a d3 exmaple by Mike Bostock)

--- tulip-python/simple_webserver.py
+++ tulip-python/simple_webserver.py
@@ -11,6 +11,9 @@ from tulip import *
 
 
 class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+
+    graph = tlp.newGraph()
+ 
     def do_GET(self):
 	print "path=",self.path
         if len(self.path.split('?'))<2:
@@ -28,7 +31,8 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			print params["n"][0]
                         #self.path = "/reponse2.json"
 			try:
-				returnJSON = graphToJSON(createGraph(int(params["n"][0])))
+				self.createGraph(int(params["n"][0]))
+				returnJSON = self.graphToJSON(self.graph)
                                 print "this is to return: ",returnJSON
 
 			except:
@@ -57,41 +61,63 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
         self.wfile.write(returnJSON)
 
         #return SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
-	
+
+    def do_POST(self):
+	ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+	print ctype
+	print pdict
+	if ctype == 'application/x-www-form-urlencoded':
+        	length = int(self.headers.getheader('content-length'))
+        	postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+		graphJSON = ""
+		if 'json_string' in postvars.keys():
+			graphJSON = json.loads(postvars['json_string'][0])
+
+		self.JSONtoGraph(graphJSON)
+
+    def JSONtoGraph(self, json):
+	nodeList = []
+	graphNList = []
+	for n in json[u'nodes']:
+		nodeList.append(n[u'name'])
+	for n in self.graph.getNodes():
+		if n.id in nodeList:
+			graphNList.append(n)
+
+	g = self.graph.inducedSubGraph(graphNList)
+	print graphToJSON(g)
         
 
 
-def createGraph(nbNodes):
-	g = tlp.newGraph()
-	nodeList = [g.addNode() for i in range(nbNodes)]
-	edgeList = [g.addEdge(nodeList[i],nodeList[j]) for j in range(nbNodes) for i in range(0,nbNodes)]#j+1, n)]
+    def createGraph(self, nbNodes):
+
+	nodeList = [self.graph.addNode() for i in range(nbNodes)]
+	edgeList = [self.graph.addEdge(nodeList[i],nodeList[j]) for j in range(nbNodes) for i in range(0,nbNodes)]#j+1, n)]
 	#print nodeList
 	#print edgeList
-	tlp.SimpleTest.makeSimple(g)
+	tlp.SimpleTest.makeSimple(self.graph)
         #print [e for e in g.getEdges()]
-	viewL = g.getLayoutProperty("viewLayout")
-	#for n in g.getNodes():
-	#	print viewL[n]
-	g.computeLayoutProperty("GEM (Frick)", viewL)
+	viewL = self.graph.getLayoutProperty("viewLayout")
+	for n in self.graph.getNodes():
+		print viewL[n]
+	self.graph.computeLayoutProperty("Circular", viewL)
 
-	#for n in g.getNodes():
-	#	print viewL[n]
-        return g
+	for n in self.graph.getNodes():
+		viewL[n] *= 10
 
-class tulipNode:
-    def __init__(_id, _x, _y):
-        self.id = _id
-        self.x = _x
-        self.y = _y
 
-def graphToJSON(graph):
-    vLayout = graph.getLayoutProperty("viewLayout")
-    nList = {"nodes":[{"name":n.id,"x":vLayout[n][0],"y":vLayout[n][1]} for n in graph.getNodes()]}
-    nToI = {nList["nodes"][i]["name"]:i for i in range(len(nList["nodes"]))}
-    eList = {"links":[{"source":nToI[graph.source(e).id], "target":nToI[graph.target(e).id], "value":1} for e in graph.getEdges()]}
-    nList.update(eList)
-    print "dumping: ", json.dumps(nList)
-    return json.dumps(nList)
+        return self.graph
+
+
+
+    def graphToJSON(self,graph):
+ 	vLayout = graph.getLayoutProperty("viewLayout")
+    	nList = {"nodes":[{"name":n.id,"x":vLayout[n][0],"y":vLayout[n][1]} for n in graph.getNodes()]}
+    	nToI = {nList["nodes"][i]["name"]:i for i in range(len(nList["nodes"]))}
+    	eList = {"links":[{"source":nToI[graph.source(e).id], "target":nToI[graph.target(e).id], "value":1} for e in graph.getEdges()]}
+    	nList.update(eList)
+    	print "dumping: ", json.dumps(nList)
+    	return json.dumps(nList)
 		
 
 def main():
