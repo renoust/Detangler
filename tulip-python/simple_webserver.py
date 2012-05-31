f3cbec61cc99eaa19245e825975386525e704a03
Benjamin Renoust <renoust@gmail.com>
2012-05-30 23:15:41 -0400

    request parameters management in do_POST (python side) now loads a given d3.layout.force compatible JSON file some cleaning in the POC file

--- tulip-python/simple_webserver.py
+++ tulip-python/simple_webserver.py
@@ -26,7 +26,10 @@ class graphManager():
 	g = self.graph.inducedSubGraph(graphNList)
 	return g
 
-    def modifyGraph(self, graph):
+    def modifyGraph(self, graph=0):
+	if not graph:
+		graph = self.graph
+
 	viewL = graph.getLayoutProperty("viewLayout")
 	graph.computeLayoutProperty("Random", viewL)
 	graph.computeLayoutProperty("Circular", viewL)
@@ -36,17 +39,75 @@ class graphManager():
 	return graph
 
 
+    ''' here we should add protection for properties automatic load (warning will crash when diff type w/ same name)
+    '''
+    def addGraph(self, json):
+	g = tlp.newGraph()
+	
+	#for d3.force.layout import
+	idToNode = {}
+        idIndex = 0
+
+	for n in json[u'nodes']:
+		u = g.addNode()
+		idToNode[idIndex] = u
+		idIndex += 1
+		for k in n.keys():
+			prop = 0
+			kType = type(n[k])
+			if kType == int or kType == float:
+				prop = g.getDoubleProperty(k.encode("utf-8"))
+			if kType == str:
+				prop = g.getStringProperty(k.encode("utf-8"))
+			if kType == bool:
+				prop = g.getBooleanProperty(k.encode("utf-8"))
+			if prop:
+				prop[u] = n[k]
+		if u'id' not in n.keys():
+			prop = g.getDoubleProperty("id")
+			prop[u] = u.id
+		
+	
+	for e in json[u'links']:
+		if u'source' in e.keys() and u'target' in e.keys():
+			print e
+			v = g.addEdge(idToNode[e[u'source']], idToNode[e[u'target']])
+
+			for k in e.keys():
+				if k not in [u'source', u'target']:
+					prop = 0
+					kType = type(e[k])
+					if kType == int or kType == float:
+						prop = g.getDoubleProperty(k.encode("utf-8"))
+					if kType == str:
+						prop = g.getStringProperty(k.encode("utf-8"))
+					if kType == bool:
+						prop = g.getBooleanProperty(k.encode("utf-8"))
+					if prop:
+						prop[v] = e[k]	
+
+			if 'id' not in e.keys():
+				prop = g.getDoubleProperty("id")
+				prop[v] = v.id
+
+	self.graph = g #temporary, we should manage sessions and graphIDs
+	return g
+
+
+	
+
     def createGraph(self, nbNodes):
+	
+	if not self.graph.numberOfNodes():
+		nodeList = [self.graph.addNode() for i in range(nbNodes)]
+		edgeList = [self.graph.addEdge(nodeList[i],nodeList[j]) for j in range(nbNodes) for i in range(0,nbNodes)]#j+1, n)]
+		tlp.SimpleTest.makeSimple(self.graph)
+		viewL = self.graph.getLayoutProperty("viewLayout")
+		self.graph.computeLayoutProperty("Random", viewL)
+		self.graph.computeLayoutProperty("Circular", viewL)
 
-	nodeList = [self.graph.addNode() for i in range(nbNodes)]
-	edgeList = [self.graph.addEdge(nodeList[i],nodeList[j]) for j in range(nbNodes) for i in range(0,nbNodes)]#j+1, n)]
-	tlp.SimpleTest.makeSimple(self.graph)
-	viewL = self.graph.getLayoutProperty("viewLayout")
-	self.graph.computeLayoutProperty("Random", viewL)
-	self.graph.computeLayoutProperty("Circular", viewL)
-
-	for n in self.graph.getNodes():
-		viewL[n] *= 10
+		for n in self.graph.getNodes():
+			viewL[n] *= 10
 
         return self.graph
 
@@ -104,14 +165,34 @@ class MyRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
 	if ctype == 'application/x-www-form-urlencoded':
         	length = int(self.headers.getheader('content-length'))
         	postvars = cgi.parse_qs(self.rfile.read(length), keep_blank_values=1)
+		print postvars.keys()
 		graphJSON = ""
-		if 'json_string' in postvars.keys():
-			graphJSON = json.loads(postvars['json_string'][0])
 
-		g = self.graphMan.JSONtoGraph(graphJSON)
-		g = self.graphMan.modifyGraph(g)
-		graphJSON = self.graphMan.graphToJSON(g)
-		self.sendJSON(graphJSON)
+		if 'type' in postvars.keys():
+			if postvars['type'][0] == 'creation':
+		
+				if 'graph' in postvars.keys():
+					#print postvars['graph'][0]
+					graphJSON = json.loads(postvars['graph'][0])
+
+					#if 'json_string' in postvars.keys():
+					#	graphJSON = json.loads(postvars['json_string'][0])
+					g = self.graphMan.addGraph(graphJSON)
+					vL = g.getLayoutProperty("viewLayout")
+					g.computeLayoutProperty("FM^3 (OGDF)", vL)
+					graphJSON = self.graphMan.graphToJSON(g)
+					self.sendJSON(graphJSON)
+
+
+			if postvars['type'][0] == 'algorithm':
+					print 'algo requested'
+
+
+			if postvars['type'][0] == 'update':
+					g = self.graphMan.JSONtoGraph(graphJSON)
+					g = self.graphMan.modifyGraph(g)
+					graphJSON = self.graphMan.graphToJSON(g)
+					self.sendJSON(graphJSON)
 
     def sendJSON(self,json):
 	self.send_response(200)
