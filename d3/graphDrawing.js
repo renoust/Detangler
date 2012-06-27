e51ad530e7a0e90a9922390d08bb6f021e9c9444
Benjamin Renoust <renoust@gmail.com>
2012-06-27 23:12:46 +0200

    synchronized selection by (rectangular) brushing (still buggy, but it might come from the output data of synchro)

--- d3/graphDrawing.js
+++ d3/graphDrawing.js
@@ -10,63 +10,12 @@ var graphDrawing = function(_graph, _svg)
 		//console.log("drawing....", g.cGraph.links())
 		g.drawLinks()
 		g.drawNodes()
-		g.addInteraction()
+		//g.addInteraction()
 	}
 
-	g.addInteraction = function()
+	g.drawShape = function()
 	{
-
-		var h = g.svg.attr("height")
-		var w = g.svg.attr("width")
-		var buttonWidth = 131
-		
-		var xScale = d3.scale.linear().range([buttonWidth, w])
-		var yScale = d3.scale.linear().range([0,h])
-
-		console.log("svg element: ",g.svg, w, h)
-		var node = g.svg.selectAll("g.node")
-
-		g.svg.append("g")
-		    .attr("class", "brush")
-		    .call(d3.svg.brush().x(xScale).y(yScale)
-		    .on("brushstart", brushstart)
-		    .on("brush", brushmove)
-		    .on("brushend", brushend))
-		    .style('stroke', 'black')
-		    .style('stroke-width', 2)
-		    .style('fill-opacity', .125)
-		    .style('shape-rendering', 'crispEdges')
-
-
-
-		function brushstart() {
-		  g.svg.classed("selecting", true);
-		}
-
-		function brushmove() {
-		  var e = d3.event.target.extent();
-		  node.classed("selected", function(d) {
-			//console.log('object d ',d);
-			//console.log('pos (',e,') against (',d.x/w,',',d.y/h);
-		    wNorm = w - buttonWidth
-		    d.selected = e[0][0] <= (d.x-buttonWidth+1)/wNorm && (d.x-buttonWidth+1)/wNorm <= e[1][0]
-			&& e[0][1] <= d.y/h && d.y/h <= e[1][1];
-		    return d.selected;
-		  }).select("circle.node").style('fill', function(d){
-			if (d.selected)
-			{return 'red';
-			}else{
-			return 'steelblue';}
-		})
-		  
-		}
-
-		function brushend() {
-		  g.svg.classed("selecting", !d3.event.target.empty());
-		}
-
-
-
+		console.log("do something")
 	}
 
 
@@ -80,13 +29,15 @@ var graphDrawing = function(_graph, _svg)
 			.attr("class", "node")
 			.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
 
-			.on("click", function(){
+			.on("click", function(d){
 				var o = d3.select(this); 
 				if (o.classed("selected"))
 				{
+					d.selected = 1
 					o.classed("selected",0)
 					o.select("circle").style("fill","steelblue");
 				}else{
+					d.selected = 0
 					o.classed("selected",1)
 					o.select("circle").style("fill","red");
 				}
@@ -193,6 +144,11 @@ var graphDrawing = function(_graph, _svg)
 	g.show = function(_graph, dTime)
 	{
 		//g.cGraph = _graph
+
+		var node = g.svg.selectAll("g.node")
+			.select("circle.node")
+			.style('fill', 'steelblue')
+			.attr('r', 5)
 
 		var node = g.svg.selectAll("g.node")
 			.data(g.cGraph.nodes(),function(d){return d.baseID})
--- d3/tulipPosyVast.js
+++ d3/tulipPosyVast.js
@@ -584,7 +584,109 @@ var TulipPosy = function(originalJSON)
 
 	}
 
+	var addBrush = function(target)
+	{
+		var svg = null
+		var graph = null
 
+		if (target == "catalyst")
+		{
+			svg = svg_catalyst
+			graph = graph_catalyst			
+		}
+	
+		if (target == "substrate")
+		{
+			svg = svg_substrate
+			graph = graph_substrate
+		}
+			
+		if (!target)
+			return
+
+		var h = svg.attr("height")
+		var w = svg.attr("width")
+		var buttonWidth = 131
+		
+		var xScale = d3.scale.linear().range([buttonWidth, w])
+		var yScale = d3.scale.linear().range([0,h])
+
+		console.log("svg element: ",svg, w, h)
+		
+
+		var brush = svg.append("g")
+		    .attr("class", "brush"+target)
+		    .call(d3.svg.brush().x(xScale).y(yScale)
+		    .on("brushstart", brushstart)
+		    .on("brush", brushmove)
+		    .on("brushend", brushend))
+		    .style('stroke', 'black')
+		    .style('stroke-width', 2)
+		    .style('fill-opacity', .125)
+		    .style('shape-rendering', 'crispEdges')
+
+
+
+		function brushstart() {
+		  svg.classed("selecting", true);
+		}
+
+		var prevSelList = [];
+
+		function brushmove() {
+		  var e = d3.event.target.extent();
+		  var node = svg.selectAll("g.node")
+		  var selList = []
+		  node.classed("selected", function(d) {
+			//console.log('object d ',d);
+			//console.log('pos (',e,') against (',d.x/w,',',d.y/h);
+		    wNorm = w - buttonWidth
+		    d.selected = e[0][0] <= (d.x-buttonWidth+1)/wNorm && (d.x-buttonWidth+1)/wNorm <= e[1][0]
+			&& e[0][1] <= d.y/h && d.y/h <= e[1][1];
+		    return d.selected;
+		  }).select("circle.node").style('fill', function(d){
+			if (d.selected)
+			{ selList.push(d.baseID); return 'red';}
+			return 'steelblue';
+		})
+
+		selList.sort()
+		if(selList.length>0)
+		{	
+			if(selList.length == prevSelList.length)
+			{
+				var i = 0;
+				var iMax = selList.length;
+				while(i<iMax && selList[i] == prevSelList[i])
+					i++;
+				if (i != iMax)
+				{
+					prevSelList.length = 0
+					prevSelList = selList.slice(0);
+					syncGraph(getSelection(target), target)
+				}
+			}else{
+					
+					prevSelList.length = 0
+					prevSelList = selList.slice(0);
+					syncGraph(getSelection(target), target)
+			}
+		}
+				
+			
+		
+		
+		  //syncGraph(getSelection(target), target)
+		//console.log(nbSelected, 'elements selected')
+		}
+
+		function brushend() {
+		  svg.classed("selecting", !d3.event.target.empty());
+		}
+
+
+
+	}
 
 
 	var loadData = function(json)
@@ -611,6 +713,7 @@ var TulipPosy = function(originalJSON)
 				//console.log('sending to tulip... :')
 				//console.log(jsonData)
 				createTulipGraph(jsonData)
+				analyseGraph()
 			});
 		}
 		else
@@ -626,11 +729,16 @@ var TulipPosy = function(originalJSON)
 			json = JSON.stringify(data)
 			loadJSON(data)
 			createTulipGraph(json)
+			analyseGraph()
 		}
 	}
+
 	addInterfaceSubstrate();
 	addInterfaceCatalyst();
 	loadData(originalJSON);
+	addBrush("substrate");
+	addBrush("catalyst");
+	
 	
 
 
