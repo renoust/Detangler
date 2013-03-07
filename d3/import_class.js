1f4188430466af00acff13d1f1b557ffe628da9b
Benjamin Renoust <renoust@gmail.com>
2013-03-08 00:15:55 +0100

    cleaner import: to do: exceptions management

--- d3/import_class.js
+++ d3/import_class.js
@@ -1,58 +1,61 @@
 // here's import function
 
-var imported = {};
-var importing = {};
+(function(){
 
-
-function import_class(url, namespace) {
-
-    function fetch_js (url)
-    {
-        var recieved_text= "null;";
-        //maybe we should restrain the list url possible, or the format of url (for sake of security)
-
-        function reqListener () {
-          recieved_text = this.responseText;
-           //console.log(recieved_text);
-        }
-
-        var oReq = new XMLHttpRequest();
-        oReq.onload = reqListener;
-        oReq.open("get", url, false);
-        oReq.send();
-        return recieved_text;
-
-    }
-
-    function extend_object(object1, object2)
-    {
-        for(var key in object2)
-        {
-            object1[key] = object2[key]
-        }
-        return object1
-    }
-
-    if (!(url in imported) && !(url in importing)) {
-    	importing[url] = "processing";
-        // fetch content of the URL, should be synchronized
-        content = fetch_js(url);
-        //console.log('the content:', content)
-        eval('var __s__ = ' + content); // __s__ has content execution results now
-        //console.log('in namespace', __s__);
-        if (namespace) {
-            if (!(namespace in window)) {
-                window[namespace] = {};
-            }
-            // extend as in python
-            extend_object( window[namespace], __s__);
-            //console.log(window[namespace]);
-        }
-        imported[url] = __s__;
-        importing["url"] = "done";
-    }
-    return imported[url]
-}
+	this.imported = {};
+	this.importing = {};
+	
+	
+	this.import_class = function(url, namespace) {
+	
+	    function fetch_js (url)
+	    {
+	        var recieved_text= "null;";
+	        //maybe we should restrain the list url possible, or the format of url (for sake of security)
+	
+	        function reqListener () {
+	          recieved_text = this.responseText;
+	           //console.log(recieved_text);
+	        }
+	
+	        var oReq = new XMLHttpRequest();
+	        oReq.onload = reqListener;
+	        oReq.open("get", url, false);
+	        oReq.send();
+	        return recieved_text;
+	
+	    }
+	
+	    function extend_object(object1, object2)
+	    {
+	        for(var key in object2)
+	        {
+	            object1[key] = object2[key]
+	        }
+	        return object1
+	    }
+	
+	    if (!(url in imported) && !(url in importing)) {
+	    	importing[url] = "processing";
+	        // fetch content of the URL, should be synchronized
+	        content = fetch_js(url);
+	        //console.log('the content:', content)
+	        eval('var __s__ = ' + content); // __s__ has content execution results now
+	        //console.log('in namespace', __s__);
+	        if (namespace) {
+	            if (!(namespace in window)) {
+	                window[namespace] = {};
+	            }
+	            // extend as in python
+	            extend_object( window[namespace], __s__);
+	            //console.log(window[namespace]);
+	        }
+	        imported[url] = __s__;
+	        importing["url"] = "done";
+	    }
+	    return imported[url]
+	}
+})();
 
 /*
 // /ui/button.js
