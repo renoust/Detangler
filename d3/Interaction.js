0177b1f71952ec2512edfb7b67c7795afa2540c8
amesure <bluewolf33@hotmail.fr>
2013-07-10 12:29:13 +0200

    Fixes #58

--- d3/Interaction.js
+++ d3/Interaction.js
@@ -810,10 +810,10 @@ this.runZoom = function(event){
         	console.log(target)
             if (TP.Context().tabOperator[target] == "OR") { //befrore, there was only catalyst
                 TP.Context().tabOperator[target] = "AND";
-                $('.ui-accordion-header').each(function(){if($(this).text()==='Operator OR'){$(this).text('Operator AND')}})
+                $('li.form > a').each(function(){if($(this).text()==='Operator OR'){$(this).text('Operator AND')}})
             } else {
                 TP.Context().tabOperator[target] = "OR"
-                $('.ui-accordion-header').each(function(){if($(this).text()==='Operator AND'){$(this).text('Operator OR')}})
+                $('li.form > a').each(function(){if($(this).text()==='Operator AND'){$(this).text('Operator OR')}})
             }
             /*
             TP.Context().view[target].getSvg().selectAll("g.toggleCatalystOp")
